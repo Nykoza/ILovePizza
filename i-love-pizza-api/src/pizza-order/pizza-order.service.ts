45f8c4e64bc33c346d56ef99313816d5b026c32d
Nykoza <nicolas.goepp@epitech.eu>
2023-12-15 23:05:41 +0100

    API(fix): comment not working code

--- i-love-pizza-api/src/pizza-order/pizza-order.service.ts
+++ i-love-pizza-api/src/pizza-order/pizza-order.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { CreatePizzaOrderDto } from './dto/create-pizza-order.dto';
-import { User } from '@prisma/client';
 
 @Injectable()
 export class PizzaOrderService {
   constructor(private prisma: PrismaService) {}
 
-  async createPizzaOrder(user: User, dto: CreatePizzaOrderDto) {
-    const pizzaOrder = await this.prisma.pizzaOrder.create({
-      data: { name: dto.name, extra: dto.extra, user: user, userId: user.id },
-    });
-
-    return pizzaOrder;
-  }
+  // async createPizzaOrder(user: User, dto: CreatePizzaOrderDto) {
+  //   // const pizzaOrder = await this.prisma.pizzaOrder.create({
+  //   //   data: { name: dto.name, extra: dto.extra, user: user, userId: user.id },
+  //   // });
+  //   //
+  //   // return pizzaOrder;
+  // }
 }
