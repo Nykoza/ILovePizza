f8c0eab960458c353c88540955aafc7f2051f6f1
Nykoza <nicolas.goepp@epitech.eu>
2023-12-30 17:45:41 +0100

    FRONT(feature): Local storage service

--- i-love-pizza-front-angular/src/app/services/auth/auth.service.ts
+++ i-love-pizza-front-angular/src/app/services/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { Observable, of } from 'rxjs';
 import { RequestClientService } from '../request-client/request-client.service';
 import { Auth, AuthPayload } from './auth';
 import { catchError, tap } from 'rxjs/operators';
+import { LocalStorageService } from '../local-storage/local-storage.service';
 
 @Injectable({
   providedIn: 'root',
@@ -12,22 +13,35 @@ export class AuthService {
   constructor(
     private messageService: MessageService,
     private requestService: RequestClientService,
+    private localStorageService: LocalStorageService,
   ) {}
 
   signIn(email: string, password: string): Observable<any> {
     const body: AuthPayload = { email: email, password };
-    return this.requestService.postUnsecured('/auth/signin', body).pipe(
-      tap((_) => this.log(`Signin with email=${email}`)),
-      catchError(this.handleError<Auth>('SignIn')),
-    );
+    return this.requestService
+      .postUnsecured<Auth, any>('/auth/signin', body)
+      .pipe(
+        tap((auth) =>
+          this.localStorageService.set('access_toke', auth.access_token),
+        ),
+        catchError(this.handleError<Auth>('SignIn')),
+      );
   }
 
   signUp(email: string, password: string): Observable<any> {
     const body: AuthPayload = { email: email, password };
-    return this.requestService.postUnsecured('/auth/signup', body).pipe(
-      tap((_) => this.log(`Signup with email=${email}`)),
-      catchError(this.handleError<Auth>('SignUp')),
-    );
+    return this.requestService
+      .postUnsecured<Auth, any>('/auth/signup', body)
+      .pipe(
+        tap((auth) =>
+          this.localStorageService.set('access_toke', auth.access_token),
+        ),
+        catchError(this.handleError<Auth>('SignUp')),
+      );
+  }
+
+  getAccessToken() {
+    return this.localStorageService.get('access_token');
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
